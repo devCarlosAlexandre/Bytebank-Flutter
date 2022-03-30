a6d75d541bb4e2b30c48f98de54e8bec4e2e0510
devCarlosAlexandre <carloalexandresousa@gmail.com.br>
2022-03-30 01:50:37 -0300

    correção da lista

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,25 @@ import 'package:flutter/material.dart';
 void main() => runApp(
 MaterialApp(
   home: Scaffold(
+    body:Column(children: <Widget>[ 
+      Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text('100.00'),
+        subtitle:Text ("conta:2058-95"),
+      ),
+    ),
+    Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text('1,250.00'),
+        subtitle:Text ("conta:2058-95"),
+      ),
+    ),
+    ],
+    ),
     appBar: AppBar(
-      title: Text("Transferencias"),
+      title: Text("Transferências"),
     ),
     floatingActionButton: FloatingActionButton(
           onPressed: () {  },//é necessário passar o parâmetro para rodar

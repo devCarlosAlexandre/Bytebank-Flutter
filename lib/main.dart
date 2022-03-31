ad96ecf77f120829d77ec7e5835ae906fb27b42e
devCarlosAlexandre <carloalexandresousa@gmail.com.br>
2022-03-30 22:47:19 -0300

    Refatoraçãodo da fução de criar transferencias

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-MaterialApp(
-  home: Scaffold(
-    body:Column(children: <Widget>[ 
-      Card(
-      child: ListTile(
-        leading: Icon(Icons.monetization_on),
-        title: Text('100.00'),
-        subtitle:Text ("conta:2058-95"),
-      ),
-    ),
-    Card(
-      child: ListTile(
-        leading: Icon(Icons.monetization_on),
-        title: Text('1,250.00'),
-        subtitle:Text ("conta:2058-95"),
-      ),
-    ),
-    ],
-    ),
-    appBar: AppBar(
-      title: Text("Transferências"),
-    ),
-    floatingActionButton: FloatingActionButton(
-          onPressed: () {  },//é necessário passar o parâmetro para rodar
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencias(),
+        appBar: AppBar(
+          title: Text("Transferências"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {}, //é necessário passar o parâmetro para rodar
           child: Icon(Icons.add),
-      ),  
-  ),
-));
+        ),
+      ),
+    ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransfeerncia(Transferencia(100.0, 154)),
+        ItemTransfeerncia(Transferencia(1050.90 , 0123)),
+        ItemTransfeerncia(Transferencia(158.59, 508)),
+        ItemTransfeerncia(Transferencia(230.00, 1974)),
+      ],
+    );
+  }
+}
+
+class ItemTransfeerncia extends StatelessWidget {
+  final Transferencia transferencia;
+  ItemTransfeerncia(this.transferencia,);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(transferencia.valor.toString()),
+        subtitle: Text(transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia{
+  final double valor;
+  final int numeroConta;
+  Transferencia(this.valor, this.numeroConta);
+}

5880eaaef1bbe78e68847fdb9e54ca1d0fa79fc5
devCarlosAlexandre <carloalexandresousa@gmail.com.br>
2022-03-30 23:55:04 -0300

    Estrutura do formulario

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(Bytebankapp());
+
+class Bytebankapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: ListaTransferencias(),
-        appBar: AppBar(
-          title: Text("Transferências"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {}, //é necessário passar o parâmetro para rodar
-          child: Icon(Icons.add),
-        ),
+        body: FormularioTransferencia(),
       ),
-    ));
+    );
+  }
+}
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransfeerncia(Transferencia(100.0, 154)),
-        ItemTransfeerncia(Transferencia(1050.90 , 0123)),
-        ItemTransfeerncia(Transferencia(158.59, 508)),
-        ItemTransfeerncia(Transferencia(230.00, 1974)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Transferências"),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransfeerncia(Transferencia(100.0, 154)),
+          ItemTransfeerncia(Transferencia(1050.90, 0123)),
+          ItemTransfeerncia(Transferencia(158.59, 508)),
+          ItemTransfeerncia(Transferencia(230.00, 1974)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
 
 class ItemTransfeerncia extends StatelessWidget {
   final Transferencia transferencia;
-  ItemTransfeerncia(this.transferencia,);
+  ItemTransfeerncia(
+    this.transferencia,
+  );
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -42,8 +53,18 @@ class ItemTransfeerncia extends StatelessWidget {
   }
 }
 
-class Transferencia{
+class Transferencia {
   final double valor;
   final int numeroConta;
   Transferencia(this.valor, this.numeroConta);
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold (
+    appBar: AppBar(title: Text('Criando Transferências'),),
+    body:Text('Olá Seja Bem Vindo!')
+  );
+  }
 }

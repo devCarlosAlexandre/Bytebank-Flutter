8efdac6c0eab1cd929864e05fc527f83585cf2b4
devCarlosAlexandre <carloalexandresousa@gmail.com.br>
2022-04-04 23:03:45 -0300

    correcao da estracao

--- lib/main.dart
+++ lib/main.dart
@@ -60,8 +60,8 @@ class Transferencia {
   final int numeroConta;
   Transferencia(this.valor, this.numeroConta);
 
-  @override 
-  String toString(){
+  @override
+  String toString() {
     return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
   }
 }
@@ -78,7 +78,7 @@ class FormularioTransferencia extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          
+          Editor(_controladorCampoNumeroConta, 'Número da conta', "0000"),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
@@ -98,17 +98,16 @@ class FormularioTransferencia extends StatelessWidget {
               final int? numeroConta =
                   int.tryParse(_controladorCampoNumeroConta.text);
               final double? valor =
-                  double.tryParse(_controladorCampoNumeroConta.text);
+                  double.tryParse(_controladorCampoValor.text);
               if (numeroConta != null && valor != null) {
                 final transferenciaCriada = Transferencia(valor, numeroConta);
                 debugPrint(transferenciaCriada.toString());
                 Scaffold.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('$transferenciaCriada'),
-                    ),
-                  );
+                  SnackBar(
+                    content: Text('$transferenciaCriada'),
+                  ),
+                );
               }
-
             },
           ),
         ],
@@ -117,5 +116,25 @@ class FormularioTransferencia extends StatelessWidget {
   }
 }
 
+class Editor extends StatelessWidget {
+  final TextEditingController _controlador;
+  final String _rotulo;
+  final String _dica;
+Editor(this._controlador, this._dica, this._rotulo);
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: _controlador,
+        style: TextStyle(fontSize: 24.0),
+        decoration: InputDecoration(
+          labelText: _rotulo,
+          hintText: _dica,
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
+}
